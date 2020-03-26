89a8119d82942ffa8519d950523ce088bb976fc3
サトウタクミ <62207045+takumi0623eve@users.noreply.github.com>
2020-03-26 23:29:49 +0900

    Create minecraft.js

---
+++ minecraft.js
@@ -0,0 +1,24 @@
+$(function(){
+    $('.slider').slick({
+        autoplay:true,
+        playSpeed:75,
+        dots:true,
+    });
+    var topBtn = $('#page-top');
+    topBtn.hide();
+    //スクロールが100に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 300);
+        return false;
+    });
+});
